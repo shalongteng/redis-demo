b8027ce563279daaa9ae220a0032affe7ff288d7
shalongteng <1131790681@qq.com>
2020-01-07 00:09:50 +0800

    init

---
+++ src/main/java/com/xdclass/mobile/xdclassmobileredis/XdclassMobileRedisApplication.java
@@ -0,0 +1,16 @@
+package com.xdclass.mobile.xdclassmobileredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class XdclassMobileRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XdclassMobileRedisApplication.class, args);
+    }
+
+}
+
